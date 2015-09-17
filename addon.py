eb832ae0b9494f5f3a71c33c07e35b4bae9d9c38
Vd <vadyur@csodessa.com>
2015-09-17 17:50:50 +0300

    Fix filename for strm. Added loging.

--- addon.py
+++ addon.py
@@ -3,24 +3,29 @@ import xbmc
 import xbmcgui
 import xbmcvfs
  
+def make_filename(title):
+	filename = title.replace("|", "_").replace("?", "_").replace("/", "_")
+	print "context.item.savestrm: filename is: " + filename
+	return filename
+
 if __name__ == '__main__':
 	message = "Clicked on '%s'" % sys.listitem.getLabel() 
-	print message
-	print sys.listitem.getfilename()
+	print "context.item.savestrm: " + message
+	print "context.item.savestrm: " + sys.listitem.getfilename()
 	
 	video_tag = xbmc.InfoTagVideo()
 	movie_title = video_tag.getTitle()
 	movie_writer = video_tag.getWritingCredits()
 	movie_genre = video_tag.getGenre()
-	xbmc.log( "[script.cinema.experience] - movie_title: %s" % movie_title, xbmc.LOGNOTICE )
-	xbmc.log( "[script.cinema.experience] - movie_writer: %s" % movie_writer, xbmc.LOGNOTICE )
-	xbmc.log( "[script.cinema.experience] - movie_genre: %s" % movie_genre, xbmc.LOGNOTICE )
-	#print sys.listitem.getProperty('dbtype')
+	xbmc.log( "context.item.savestrm: movie_title: %s" % movie_title, xbmc.LOGNOTICE )
+	xbmc.log( "context.item.savestrm: movie_writer: %s" % movie_writer, xbmc.LOGNOTICE )
+	xbmc.log( "context.item.savestrm: movie_genre: %s" % movie_genre, xbmc.LOGNOTICE )
+	#print "context.item.savestrm: " + sys.listitem.getProperty('dbtype')
 	dialog = xbmcgui.Dialog()
 	path = dialog.browseSingle(0, 'Select directory for save .strm', 'video')
-	print path
+	print "context.item.savestrm: save to " + path
 	
-	file = path + '/' + sys.listitem.getLabel() + '.strm'
+	file = path + '/' + make_filename(sys.listitem.getLabel()) + '.strm'
 	f = xbmcvfs.File (file, 'w')
 	result = f.write(sys.listitem.getfilename())
 	f.close()
