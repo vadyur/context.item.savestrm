d304f181a7c99f2d6d8198da35cc9ea485cc42d5
Vd <vadyur@csodessa.com>
2015-09-24 11:57:20 +0300

    Refactoring

--- addon.py
+++ addon.py
@@ -2,13 +2,22 @@ import sys
 import xbmc
 import xbmcgui
 import xbmcvfs
- 
+
+##----------------------------------------------------------------------------- 
 def make_filename(title):
 	filename = title.replace("|", "_").replace("?", "_").replace("/", "_")
 	print "context.item.savestrm: filename is: " + filename
 	return filename
-
-if __name__ == '__main__':
+	
+##-----------------------------------------------------------------------------
+def save_to_file(path):
+	file = path + '/' + make_filename(sys.listitem.getLabel()) + '.strm'
+	f = xbmcvfs.File (file, 'w')
+	result = f.write(sys.listitem.getfilename())
+	f.close()
+	
+##-----------------------------------------------------------------------------
+def main():
 	message = "Clicked on '%s'" % sys.listitem.getLabel() 
 	print "context.item.savestrm: " + message
 	print "context.item.savestrm: " + sys.listitem.getfilename()
@@ -20,12 +29,13 @@ if __name__ == '__main__':
 	xbmc.log( "context.item.savestrm: movie_title: %s" % movie_title, xbmc.LOGNOTICE )
 	xbmc.log( "context.item.savestrm: movie_writer: %s" % movie_writer, xbmc.LOGNOTICE )
 	xbmc.log( "context.item.savestrm: movie_genre: %s" % movie_genre, xbmc.LOGNOTICE )
-	#print "context.item.savestrm: " + sys.listitem.getProperty('dbtype')
+
 	dialog = xbmcgui.Dialog()
 	path = dialog.browseSingle(0, 'Select directory for save .strm', 'video')
 	print "context.item.savestrm: save to " + path
 	
-	file = path + '/' + make_filename(sys.listitem.getLabel()) + '.strm'
-	f = xbmcvfs.File (file, 'w')
-	result = f.write(sys.listitem.getfilename())
-	f.close()
+	save_to_file(path)
+	
+##-----------------------------------------------------------------------------	
+if __name__ == '__main__':
+	main()
